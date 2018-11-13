8cb0f5ae3de3b5b8a942bfba16d8fd33086e47d6
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-11-13 15:00:54 +0800

    补充常量定义

--- lichkin-projects-activiti-common/src/main/java/com/lichkin/defines/ActivitiStatics.java
+++ lichkin-projects-activiti-common/src/main/java/com/lichkin/defines/ActivitiStatics.java
@@ -1,10 +1,15 @@
 package com.lichkin.defines;
 
+import com.lichkin.framework.defines.LKFrameworkStatics;
+
 /**
  * 常量定义
  * @author SuZhou LichKin Information Technology Co., Ltd.
  */
 public interface ActivitiStatics {
+
+	/** 审批流地址 */
+	public static final String ACTIVITI_CENTER_URL = CoreStatics.USER_SSO_URL + "/activiti/index" + LKFrameworkStatics.WEB_MAPPING_PAGES;
 
 	/** 流程配置表主键 */
 	public static final String KEY_PROCESS_CONFIG_ID = "processConfigId";
