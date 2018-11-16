fea9bb3321c7d30f7b2193c4c2f85a2fef3943c0
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-11-16 15:10:18 +0800

    单点登录地址定义修改

--- lichkin-projects-activiti-common/src/main/java/com/lichkin/defines/ActivitiStatics.java
+++ lichkin-projects-activiti-common/src/main/java/com/lichkin/defines/ActivitiStatics.java
@@ -1,7 +1,5 @@
 package com.lichkin.defines;
 
-import com.lichkin.framework.defines.LKFrameworkStatics;
-
 /**
  * 常量定义
  * @author SuZhou LichKin Information Technology Co., Ltd.
@@ -9,7 +7,7 @@ import com.lichkin.framework.defines.LKFrameworkStatics;
 public interface ActivitiStatics {
 
 	/** 审批流地址 */
-	public static final String ACTIVITI_CENTER_URL = CoreStatics.EMPLOYEE_SSO_URL + "/employee/activiti/index" + LKFrameworkStatics.WEB_MAPPING_PAGES;
+	public static final String ACTIVITI_CENTER_URL = "/activiti";
 
 	/** 流程配置表主键 */
 	public static final String KEY_PROCESS_CONFIG_ID = "processConfigId";
